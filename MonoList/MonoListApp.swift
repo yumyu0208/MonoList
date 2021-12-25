afe74c63d4b905ccbae0b9561e72aed07206512c
Yuma Takeda <yumyu0208@gmail.com>
2021-12-25 11:37:11 +0900

    Initial Commit

---
+++ MonoList/MonoListApp.swift
@@ -0,0 +1,20 @@
+//
+//  MonoListApp.swift
+//  MonoList
+//
+//  Created by 竹田悠真 on 2021/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MonoListApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
